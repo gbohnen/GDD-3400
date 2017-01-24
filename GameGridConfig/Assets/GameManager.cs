e0d1b528d1a3b20d9d8238504f967a373ea9f9fc
gbohnen <fezmonkey@live.com>
2017-01-24 14:51:13 -0700

    Basic Grid Stuff

---
+++ GameGridConfig/Assets/GameManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject tilePrefab;
+    public GridCell[,] graph;
+
+    int graphWidth = 25;
+    int graphHeight = 25;
+    int timer;
+
+    void Start()
+    {
+        graph = new GridCell[graphWidth, graphHeight];
+
+        for (int i = 0; i < graphWidth; i++)
+        {
+            for (int j = 0; j < graphHeight; j++)
+            {
+                graph[i, j] = Instantiate(tilePrefab).GetComponent<GridCell>();
+
+                graph[i, j].gameObject.transform.position = new Vector3(i, 0, j);
+                graph[i, j].Initialize();
+            }
+        }
+
+        ComputeNeighbors();
+    }
+
+    void ComputeNeighbors()
+    {
+        for (int i = 0; i < graphWidth; i++)
+        {
+            for (int j = 0; j < graphHeight; j++)
+            {
+                // check corners
+                // bottom-left
+                if (i == 0 && j == 0)
+                {
+                    graph[i, j].Neighbors.Add(graph[i + 1, j]);
+                    graph[i, j].Neighbors.Add(graph[i, j + 1]);
+                }
+                // top-left
+                if (i == 0 && j == graphHeight)
+                {
+                    graph[i, j].Neighbors.Add(graph[i + 1, j]);
+                    graph[i, j].Neighbors.Add(graph[i, j - 1]);
+                }
+                // bottom-right
+                if (i == graphWidth && j == 0)
+                {
+                    graph[i, j].Neighbors.Add(graph[i - 1, j]);
+                    graph[i, j].Neighbors.Add(graph[i, j + 1]);
+                }
+                // top-right
+                if (i == graphWidth && j == graphHeight)
+                {
+                    graph[i, j].Neighbors.Add(graph[i - 1, j]);
+                    graph[i, j].Neighbors.Add(graph[i, j - 1]);
+                }
+            }
+        }
+    }
+
+}
---
+++ GameGridConfig/Assets/GridCell.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CellState { Empty = 0, Occupied = 1, Targeted = 2 }
+
+public class GridCell: MonoBehaviour {
+
+    #region Methods
+
+    void Update()
+    {
+        // update cell color
+        switch (State)
+        {
+            case CellState.Empty:
+                gameObject.GetComponent<MeshRenderer>().material.color = Color.white;
+                break;
+            case CellState.Occupied:
+                gameObject.GetComponent<MeshRenderer>().material.color = Color.blue;
+                break;
+            case CellState.Targeted:
+                gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
+                break;
+        }
+    }
+
+    public void Initialize()
+    {
+        State = CellState.Empty;
+        Neighbors = new List<GridCell>();
+    }
+
+    #endregion
+
+    #region Properties
+
+    public CellState State
+    { get; set; }
+
+    public List<GridCell> Neighbors
+    { get; set; }
+
+    #endregion
+
+}
